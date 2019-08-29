3fc2be7e7df07ea5b556ac55ee3b5fcabc1333f1
Gerson0409 <alasgerson9@gmail.com>
2019-08-28 21:48:02 -0600

    primer comit

---
+++ Ejemplo String/src/ejemplo/string/EjemploString.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo.string;
+import java .util.*;
+/**
+ *
+ * @author DELL
+ */
+public class EjemploString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Declaracion de String de diferentes maneras
+        
+        String str1=new String("En java, los String son objetos");
+        
+         String str2=new String("Se construyen de varias maneras");
+         
+          String str3=new String(str2);
+          
+          System.out.println("str1");
+          System.out.println("str2");
+          System.out.println("str3");
+        
+        
+    }
+    
+}
